d238103a977614e2ca744dc16c500c0a5f563ea2
victor.rodrigues <victor.rodrigues@metasix.com.br>
2020-07-24 14:44:55 -0300

    adding logger in the controllers

--- src/main/java/com/rodrigues/funds/api/controller/FundController.java
+++ src/main/java/com/rodrigues/funds/api/controller/FundController.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -27,53 +29,59 @@ import com.rodrigues.funds.api.service.FundService;
 @RequestMapping("/v1/funds")
 public class FundController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(FundController.class);
+	
 	@Autowired
 	private FundService fundService;
 	
 	@PostMapping
 	public ResponseEntity<FundDto> createFund (@Validated @RequestBody FundForm fundForm, UriComponentsBuilder uriBuilder) {
-		
 		Fund fund = fundService.createFund(fundForm);
 		
 		URI uri = uriBuilder.path("/v1/funds/{id}")
 				.buildAndExpand(fund.getId())
 				.toUri();
 		
+		LOGGER.info("Created a new fund. Id: " + fund.getId());
 		return ResponseEntity.created(uri).body(new FundDto(fund));
 	}
 	
 	@PutMapping("/{id}")
 	public ResponseEntity<FundDto> updateFund (@RequestBody FundToUpdateForm fundForm, @PathVariable Long id) {
-		
 		Optional<Fund> fundUpdated = fundService.updateFund(fundForm, id);
 		
-		if (fundUpdated.isPresent())
+		if (fundUpdated.isPresent()) {
+			LOGGER.info("Fund " + id + " updated");
 			return ResponseEntity.ok(new FundDto(fundUpdated.get()));
-		
+		}
+			
 		return null;
 	}
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<FundDto> getFund (@PathVariable Long id) {
-		
 		Optional<Fund> fund = fundService.getFund(id);
 		
-		if (fund.isPresent())
+		if (fund.isPresent()) {
+			LOGGER.info("Fund " + id + " finded");
 			return ResponseEntity.ok(new FundDto(fund.get()));
+		}
 		
 		return ResponseEntity.notFound().build();
 	}
 	
 	@GetMapping
 	public ResponseEntity<List<FundDto>> getAllFunds () {
+		LOGGER.info("Listing funds");
 		return ResponseEntity.ok(new FundDto().list(fundService.getAllFunds()));
 	}
 	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> deleteFund (@PathVariable Long id) {
-		
-		if (fundService.deleteFund(id))
+		if (fundService.deleteFund(id)) {
+			LOGGER.info("Fund " + id + " deleted");
 			return ResponseEntity.noContent().build();
+		}
 		
 		return ResponseEntity.notFound().build();
 	}
--- src/main/java/com/rodrigues/funds/api/controller/OperationController.java
+++ src/main/java/com/rodrigues/funds/api/controller/OperationController.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -25,6 +27,8 @@ import com.rodrigues.funds.api.service.OperationService;
 @RequestMapping("v1/operations")
 public class OperationController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(OperationController.class);
+	
 	@Autowired
 	private OperationService operationService;
 	
@@ -37,6 +41,7 @@ public class OperationController {
 				.buildAndExpand(operation.getId())
 				.toUri();
 		
+		LOGGER.info("Created a new operation. Id: " + operation.getId());
 		return ResponseEntity.created(uri).body(new OperationDto(operation));
 	}
 	
@@ -45,22 +50,27 @@ public class OperationController {
 	
 		Optional<Operation> operation = operationService.getOperation(id);
 		
-		if (operation.isPresent())
+		if (operation.isPresent()) {
+			LOGGER.info("Operation " + id + " finded");
 			return ResponseEntity.ok().body(new OperationDto(operation.get()));
+		}
 		
 		return ResponseEntity.notFound().build();
 	}
 	
 	@GetMapping
 	public ResponseEntity<List<OperationDto>> getAllOperations () {
+		LOGGER.info("Listing operations");
 		return ResponseEntity.ok().body(new OperationDto().list(operationService.getAllOperations()));
 	}
 	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> deleteOperation (@PathVariable Long id) {
 		
-		if (operationService.deleteOperation(id))
+		if (operationService.deleteOperation(id)) {
+			LOGGER.info("Operation " + id + " deleted");
 			return ResponseEntity.noContent().build();
+		}
 		
 		return ResponseEntity.notFound().build();
 	}
